a5bd3e64acaf9869094d2a345fa8e8039fa8dc0b
Quoc Hoang <tin2397@me.com>
2022-02-24 14:41:16 +0700

    Initial Commit

---
+++ Github test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github test
+//
+//  Created by Quoc Hoang on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github test/Github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_testApp.swift
+//  Github test
+//
+//  Created by Quoc Hoang on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
